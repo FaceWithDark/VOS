cecb9315001029539c6cd25abf2751fa0054248b
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2023-12-18 22:12:15 +1100

    Innited.
    - Simple login page (No database yet).
    - Archive, Song, Home & Staff webpages created.
    - Simple stylish navigation bar.

---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VOT</title>
+    <!-- Include HTML file into the current HTML website with PHP -->
+    <?php include ("./navbar.html"); ?>
+</head>
+<body>
+    <center><h1 style="background-color: orange;">This is Home page</h1></center>
+</body>
+</html>
+
+<?php
+    session_destroy()
+?>
