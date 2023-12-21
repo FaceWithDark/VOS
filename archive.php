41a427a1f28557629f637e9b5932ea40982390f6
Glovania <114546939+FaceWithDark@users.noreply.github.com>
2023-12-21 11:05:34 +1100

    Create simple Hyperlink table sheet
    - More updates:
    + Tweaking some colour for navigation bar and background to suit better.
    
    - Previous commit bug:
    + Error printed out on screen because the login section cannot be processed --> FIXED (click on the login button).
    
    //TODO: make it responsive.

--- archive.php
+++ archive.php
@@ -1,4 +1,5 @@
 <?php
+    include 'navigation_bar.html';
     // Start the on-going session throughtout all other webpages (stored within' the server's cookie sessions //TODO: need to implement cookies).
     session_start()
 ?>
@@ -9,10 +10,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VOT</title>
-
-    <?php include('navigation_bar.html'); ?>
 </head>
-<body style="background-color: #008b8b;">
+<!-- Set <body> tag background color -->
+<body style="background-color: #4c516d">
     <form action="archive.php" method="post">
         <!-- Webpage's main title name -->
         <div class="title">Archived Tournament</div>
@@ -26,16 +26,43 @@
                 margin: 15px;
             }
         </style>
-        <!-- Create navigation class -->
-        <nav>
-            <!-- Create listing element where order of the items inside not matter -->
-            <ul>
-                <!-- Direct to VOT main spreadsheet -->
-                <li><a href="https://docs.google.com/spreadsheets/d/1NTKjjdmax-qjO8L9-V8Iuom8zjcCaz9X-DHFJ4SDSAg/edit#gid=0">Vietnam osu!taiko Tournament 1 </a></li>
-                <li><a href="https://docs.google.com/spreadsheets/d/1LJg5ITi8tqUer-C2dKb1xE-MYQSELpi_-_Ur2UV_las/edit#gid=0">Vietnam osu!taiko Tournament 2 </a></li>
-                <li><a href="https://docs.google.com/spreadsheets/d/17O6J2lPWZWVvozOhT3OiumRrmFn_9wRlLQrwOHP2B-k/edit#gid=0">Vietnam osu!taiko Tournament 3 </a></li>
-            </ul>
-        </nav>
+        
+        <!-- Create a table (Like table function in Excel, MS Word, etc).
+             To create border line, use "border" attribute -->
+        <table class="table">
+            <!-- Create table rows, to adjust the position then use "align" attribute -->
+            <tr>
+                <!-- Create table headers (How many vertical boxes will be needed to be create in the table row tag )
+                     To direct VOT main spreadsheets, use <a> tag -->
+                <th width="350"><a href="https://docs.google.com/spreadsheets/d/1NTKjjdmax-qjO8L9-V8Iuom8zjcCaz9X-DHFJ4SDSAg/edit#gid=0">Vietnam osu!taiko Tournament 1</a></th>
+                <th width="350"><a href="https://docs.google.com/spreadsheets/d/1LJg5ITi8tqUer-C2dKb1xE-MYQSELpi_-_Ur2UV_las/edit#gid=0">Vietnam osu!taiko Tournament 2</a></th>
+                <th width="350"><a href="https://docs.google.com/spreadsheets/d/17O6J2lPWZWVvozOhT3OiumRrmFn_9wRlLQrwOHP2B-k/edit#gid=0">Vietnam osu!taiko Tournament 3</a></th>
+                <th width="350"><a href="#">Vietnam osu!taiko Tournament 4</a></th>
+            </tr>
+
+            <!-- Create table rows, to adjust the position then use "align" attribute -->
+            <tr align="center">
+                <!-- Create table data (Un-bolded texts) -->
+                <td>Winner: crazynt_ngu</td>
+                <td>Winner: JackTVN</td>
+                <td>Winner: nahieu2005</td>
+                <td>Winner: N/A</td>
+            </tr>
+        </table>
+        
+        <style>
+            .table {
+                margin-left: auto;
+                margin-right: auto;
+                border: 2px solid;
+                border-color: black;
+            }
+
+            tr {
+                align-items: center;
+                background-color: grey;
+            }
+        </style>
     </form>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    include './navigation_bar.html';
     // Start the on-going session throughtout all other webpages (stored within' the server's cookie sessions //TODO: need to implement cookies).
     session_start();
 ?>
@@ -9,8 +10,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VOT</title>
-
-    <?php include 'navigation_bar.html'; ?>
 </head>
 <!-- Set backhround color -->
 <body style="background-color: #367588;">
@@ -29,7 +28,7 @@
                 justify-content: center;
                 color: orange;
             }
-        </style>        
+        </style>
     </form>
 </body>
 </html>
