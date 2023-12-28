d801bfa65d8e021731a6b563ed2ca414a34d113d
Glovania <114546939+FaceWithDark@users.noreply.github.com>
2023-12-28 14:28:30 +1100

    Working on "Home" page.
    - Current implement:
    + Each box will show different information respectively.
    
    + Each of them will be in the format of:
       +) Header text
       +) Sub-text (h2)
       +) Video/Logo
       +) Direct link to individuals tournament page.

--- index.php
+++ index.php
@@ -11,26 +11,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VOT</title>
 </head>
-<!-- Set backhround color -->
-<body style="background-color: #367588;">
-    <form action="index.php" method="post">
-        <!-- Website title name -->
-        <div class="title">Welcome to VOT (Vietnamese Osu!Taiko Tournament) webisite !</div>
-        <!-- Stylish ""title" class inside HTML file using CSS inline -->
-        <style>
-            .title {
-                font-family: "Montserrat", sans-serif;
-                font-size: 30px;
-                text-align: center;
-                height: calc(100vh - 60px);
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                color: orange;
-            }
-        </style>
-    </form>
-</body>
+    <!-- Stylish ""title" class inside HTML file using CSS inline -->
+    <style>
+        * {
+            font-family: "Montserrat", sans-serif;
+        }
+
+        .container {        
+            padding: 2rem;
+            display: flex;
+            flex-wrap: wrap;
+            gap: 1rem;
+        }
+
+        .cell {
+            background-color: lightgreen;
+            padding: 2rem;
+            font-weight: bold;
+        }
+    </style>
+    <!-- Set backhround color -->
+    <body style="background-color: #367588;">
+        <form action="index.php" method="post">
+            <div class="container">
+                <div class="cell">Welcome to VOT - the most enjoyable, interesting Vietnamese osu!taiko Tournament oyomyomyom !</div>
+                <div class="cell">Box 2</div>
+                <div class="cell">Box 3</div>
+            </div>
+        </form>
+    </body>
 </html>
 
 <?php
