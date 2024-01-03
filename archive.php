5111bde0c7405482d506114e584ccd729f3251b2
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2024-01-04 09:37:56 +1100

    Improve "Archive" page.
    - Styling page:
    
    + Change background colour (TEMP).
    + Change box's style and other colour implementation (TEMP).
    + Remove changing colour when hovering on top the text in the box (TEMP).
    + Tweaking some spacing distance between each elements.

--- archive.php
+++ archive.php
@@ -10,61 +10,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VOT</title>
+
+    <!-- Stylish ""title" class inside HTML file using CSS inline -->
+    <style>
+        body { 
+            font-family: "Montserrat", sans-serif;
+            background-color: #F3EEEA; 
+        }
+
+        #title {
+            font-size: 30px;
+            font-weight: 600;
+            text-align: center;
+            color: #7C9D96;
+            margin: 2rem;
+        }
+
+        table {
+            border-spacing: 2vh 10px; /* Set spacing for both horizontial and veritcal value respectively. */
+            border-collapse: separate;            
+        }
+
+        th, td {
+            border: 5px solid darkgray; /* 5px is for test debug purpose. 2px would be better. */
+            border-radius: 15px;
+            align-items: center;
+            padding: 1rem;
+            background-color: lightcoral;
+        }
+
+        th a {
+            color: lightgreen;
+        }
+    </style>
 </head>
-<!-- Set <body> tag background color -->
-<body style="background-color: #4c516d">
-    <form action="archive.php" method="post">
-        <!-- Webpage's main title name -->
-        <div class="title">Archived Tournament</div>
-        <!-- Stylish ""title" class inside HTML file using CSS inline -->
-        <style>
-            .title {
-                font-family: "Montserrat", sans-serif;
-                font-size: 30px;
-                text-align: center;
-                color: orange;
-                margin: 15px;
-            }
-        </style>
-        
-        <!-- Create a table (Like table function in Excel, MS Word, etc).
-             To create border line, use "border" attribute -->
-        <table class="table">
-            <!-- Create table rows, to adjust the position then use "align" attribute -->
-            <tr>
-                <!-- Create table headers (How many vertical boxes will be needed to be create in the table row tag )
-                     To direct VOT main spreadsheets, use <a> tag -->
-                <th width="350"><a href="https://docs.google.com/spreadsheets/d/1NTKjjdmax-qjO8L9-V8Iuom8zjcCaz9X-DHFJ4SDSAg/edit#gid=0">Vietnam osu!taiko Tournament 1</a></th>
-                <th width="350"><a href="https://docs.google.com/spreadsheets/d/1LJg5ITi8tqUer-C2dKb1xE-MYQSELpi_-_Ur2UV_las/edit#gid=0">Vietnam osu!taiko Tournament 2</a></th>
-                <th width="350"><a href="https://docs.google.com/spreadsheets/d/17O6J2lPWZWVvozOhT3OiumRrmFn_9wRlLQrwOHP2B-k/edit#gid=0">Vietnam osu!taiko Tournament 3</a></th>
-                <th width="350"><a href="#">Vietnam osu!taiko Tournament 4</a></th>
-            </tr>
-
-            <!-- Create table rows, to adjust the position then use "align" attribute -->
-            <tr align="center">
-                <!-- Create table data (Un-bolded texts) -->
-                <td>Winner: crazynt_ngu</td>
-                <td>Winner: JackTVN</td>
-                <td>Winner: nahieu2005</td>
-                <td>Winner: N/A</td>
-            </tr>
-        </table>
-        
-        <style>
-            .table {
-                margin-left: auto;
-                margin-right: auto;
-                border: 2px solid;
-                border-color: black;
-            }
-
-            tr {
-                align-items: center;
-                background-color: grey;
-            }
-        </style>
-    </form>
-</body>
+    <body>
+        <form action="archive.php" method="post">
+            <!-- Webpage's main title name -->
+            <div id="title">Archived Tournament</div>
+            <!-- Create a table (Like table function in Excel, MS Word, etc). To create border line, use "border" attribute -->
+            <table>
+                <!-- Create table rows, to adjust the position then use "align" attribute -->
+                <tr>
+                    <!-- Create table headers (How many vertical boxes will be needed to be create in the table row tag). To direct VOT main spreadsheets, use <a> tag -->
+                    <th><a href="https://docs.google.com/spreadsheets/d/1NTKjjdmax-qjO8L9-V8Iuom8zjcCaz9X-DHFJ4SDSAg/edit#gid=0">Vietnam osu!taiko Tournament 1</a></th>
+                    <th><a href="https://docs.google.com/spreadsheets/d/1LJg5ITi8tqUer-C2dKb1xE-MYQSELpi_-_Ur2UV_las/edit#gid=0">Vietnam osu!taiko Tournament 2</a></th>
+                    <th><a href="https://docs.google.com/spreadsheets/d/17O6J2lPWZWVvozOhT3OiumRrmFn_9wRlLQrwOHP2B-k/edit#gid=0">Vietnam osu!taiko Tournament 3</a></th>
+                    <th><a href="#">Vietnam osu!taiko Tournament 4</a></th>
+                </tr>
+            </table>
+        </form>
+    </body>
 </html>
 
 <?php
