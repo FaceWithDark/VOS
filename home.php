8c3065cadee772aa717a264498b4beb96a54e69e
FaceWithDark <binhsan1307@gmail.com>
2024-02-12 15:14:07 +1100

    Re-cooking & Re-ordering files and functions.
    More detail about the commit:
    + Move JS function out to a separate file for the sake of god.
    
    + Combine 'home' & 'navigation bar' CSS file into one for better debugging.
    
    + In progress of texting out responsive text next to navigation bar to fix the bug listed below.
    
    Bugs / Not working properly function:
    
    + Animation when clicked on the 'menu' icon again after click on it first time is not right. It suddenly move back, rather than slowly slide back.
    
    + Contents not responsive with the navigation bar when extend / unextend when clicked on the 'menu' icon.

--- home.php
+++ home.php
@@ -1,7 +1,3 @@
-<?php
-    include './navigation-bar.html';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,38 +5,168 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VOT</title>
 
-    <link rel="stylesheet" href="css/home.css">
+    <!-- Import CSS styling file into the current HTML file. -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Import Boxicons icons into the current HTML file. -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <style>
+        .information-box {
+            position: relative;
+            background-color: lightgreen;
+            min-height: 10vh;
+            left: 80px;
+            transition: all 0.5s ease;
+            width: calc(100% - 80px);
+            padding-top: 1rem;
+            margin: 1.5rem 0;
+            border-radius: 1rem;
+            z-index: -1;
+        }
+
+        .information-box .main-title {
+            font-weight: bold;
+            text-transform: uppercase;
+            height: 50px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .information-box .main-context {
+            background-color: lightblue;
+        }
+
+        /* .youtube-iframe-container {
+            position: relative;
+            width: 100%;
+            padding-bottom: 56.25%;
+            height: auto;
+            overflow: hidden;
+        }
+
+        .youtube-iframe-container iframe {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+        } */
+    </style>
 </head>
     <!-- Set backhround color -->
     <body style="background-color: #367588;">
         <form action="index.php" method="post">
-            <section>
-                <div class="container">
-                    <div class="box" id="box1">
-                        <div class="box-top" id="box1-top">Vietnamese osu!taiko Tournament</div>
-                        <div class="box-bottom" id="box1-bottom">Welcome to the website of the biggest vietnamese osu!taiko tournament battlefield ! In here, you can look at everything related to VOT, with pages for all our current and past tournaments!
-                            <!-- <div class="youtube-iframe-container">
-                                <iframe src="https://www.youtube.com/embed/aeUVQe7irW4?si=HmFiaAGYbrz5fCuR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div> -->
-                        </div>
-                    </div>
+            <!-- Indicate the 'navigation' part of the current HTML file. -->
+            <nav>
+                <!-- Custom class for styling the 'top part' of the navigation bar. -->
+                <div class="top-navigation-bar">
+                    <!-- Import custom Boxicons icons' class. -->
+                    <!-- Bold the wanted text (less bolded than <h1> tag). -->
+                    <i class="bx bxs-navigation"><strong>Navigate</strong></i>
+                </div>
 
-                    <div class="box" id="box2">
-                        <div class="box-top" id="box2-top">Vietnamese osu!taiko Tournament 3</div>
-                        <div class="box-bottom" id="box2-bottom">The Vietnamese osu!taiko Tournament 3 is our 3rd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</div>
-                    </div>
+                <!-- Import custom Boxicons icons' class -->
+                <!-- Custom 'id' class for styling when clicked on the 'menu' icon. -->
+                <i class="bx bx-menu" id="click-button"></i>
 
-                    <div class="box" id="box3">
-                        <div class="box-top" id="box3-top">Vietnamese osu!taiko Tournament 2</div>
-                        <div class="box-bottom" id="box3-bottom">The Vietnamese osu!taiko Tournament 2 is our 2nd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</div>
-                    </div>
+                <!-- Custom class for styling users profile. -->
+                <div class="users">
+                    <!-- Customize the current imported image to fit with the wanted navigation bar. -->
+                    <img src="#" alt="me" class="users-image">
+                    <!-- TODO: Use PHP to call player name's after API passed. -->
+                    <p>Insert name here</p>
+                </div>
+
+                <!-- Create an un-ordered list of elements. -->
+                <ul>
+                    <!-- Formatting elements into dot-point listed style. -->
+                    <li>
+                        <!-- Direct links. -->
+                        <a href="home.php">
+                            <!-- Import custom Boxicons icons' class. -->                            
+                            <i class="bx bxs-grid-alt"></i>
+                            <!-- Custom class to style 'Home' click button. -->
+                            <span class="navigation-links">Home</span>
+                        </a>
+                        <!-- Custom class to style 'Home' guide tip when re-sized 'menu' button. -->
+                        <span class="small-navigation-links">Home</span>
+                    </li>
+        
+                    <!-- Formatting elements into dot-point listed style. -->
+                    <li>
+                        <!-- Direct links. -->
+                        <a href="archive.php">
+                            <!-- Import custom Boxicons icons' class. -->
+                            <i class="bx bxs-box"></i>
+                            <!-- Custom class to style 'Archive' click button. -->
+                            <span class="navigation-links">Archive</span>
+                        </a>
+                        <!-- Custom class to style 'Archieve' guide tip when re-sized 'menu' button. -->
+                        <span class="small-navigation-links">Archive</span>
+                    </li>
                     
-                    <div class="box" id="box4">
-                        <div class="box-top" id="box4-top">Vietnamese osu!taiko Tournament 1</div>
-                        <div class="box-bottom" id="box4-bottom">The Vietnamese osu!taiko Tournament 1 is our 1st vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</div>
-                    </div>             
+                    <!-- Formatting elements into dot-point listed style. -->
+                    <li>
+                        <!-- Direct links. -->
+                        <a href="staff.php">
+                            <!-- Import custom Boxicons icons' class. -->
+                            <i class="bx bxs-phone"></i>
+                            <!-- Custom class to style 'Staff' click button. -->
+                            <span class="navigation-links">Staff</span>
+                        </a>
+                        <!-- Custom class to style 'Staff' guide tip when re-sized 'menu' button. -->
+                        <span class="small-navigation-links">Staff</span>
+                    </li>
+ 
+                    <!-- Formatting elements into dot-point listed style. -->
+                    <li>
+                        <!-- Direct links. -->
+                        <a href="song.php">
+                            <!-- Import custom Boxicons icons' class. -->
+                            <i class="bx bxs-music"></i>
+                            <!-- Custom class to style 'Song' click button. -->
+                            <span class="navigation-links">Song</span>
+                        </a>
+                        <!-- Custom class to style 'Song' guide tip when re-sized 'menu' button. -->
+                        <span class="small-navigation-links">Song</span>
+                    </li>
+                </ul>
+            </nav>
+
+            <section class="container">
+                <div class="information-box" id="annoucement-box">
+                    <div class="main-title">Vietnamese osu!taiko Tournament</div>
+                    <div class="main-context">Welcome to the website of the biggest vietnamese osu!taiko tournament battlefield ! In here, you can look at everything related to VOT, with pages for all our current and past tournaments!
+                        <!-- <div class="youtube-iframe-container">
+                            <iframe src="https://www.youtube.com/embed/aeUVQe7irW4?si=HmFiaAGYbrz5fCuR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div> -->
+                    </div>
+                </div>
+
+                <div class="information-box">
+                    <div class="main-title">Vietnamese osu!taiko Tournament 3</div>
+                    <div class="main-context">
+                        <p>The Vietnamese osu!taiko Tournament 3 is our 3rd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
+                    </div>
+                </div>
+
+                <div class="information-box">
+                    <div class="main-title">Vietnamese osu!taiko Tournament 2</div>
+                    <div class="main-context">
+                        <p>The Vietnamese osu!taiko Tournament 2 is our 2nd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
+                    </div>
+                </div>
+                
+                <div class="information-box">
+                    <div class="main-title">Vietnamese osu!taiko Tournament 1</div>
+                    <div class="main-context">
+                        <p>The Vietnamese osu!taiko Tournament 1 is our 1st vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
+                    </div>
                 </div>
             </section>
         </form>
+
+        <!-- Import JavaScript function file into the current HTML file. -->
+        <script src="activeButton.js" type="text/JavaScript"></script>
     </body>
 </html>
