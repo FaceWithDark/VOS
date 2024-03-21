417a6878d88bd9a44e0dc547e4583eea7e257040
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2024-03-21 12:04:01 +1100

    Upadte 'Archived' page
    Put JS files into a JS folder for better file management + accessibility.
    
    Comebine styling for this page specfically into the general CSS file for better maintainability.
    
    Convert from using table for archived tournament links into flexable box for better maintainability.
    
    Remove some unnecessary comments to reduce pproject's size. (TODO: There are some more needed to be remove)
    
    Remove <form> tag out of 'Home' page as it is not needed.
    
    Remove 'Test' file as it sholdn't be included to the production branch. (TODO: create a 'Test' branch exclusively with ower of the repo's access only for testing development phase).
    
    'Archived' page is now responsive with the navigation bar.

--- archive.php
+++ archive.php
@@ -9,54 +9,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VOT</title>
 
-    <!-- Stylish ""title" class inside HTML file using CSS inline -->
-    <style>
-        body { 
-            font-family: "Montserrat", sans-serif;
-            background-color: #F3EEEA; 
-        }
+    <link rel="stylesheet" href="css/style.css">
 
-        #title {
-            font-size: 30px;
-            font-weight: 600;
-            text-align: center;
-            color: #7C9D96;
-            margin: 2rem;
-        }
-
-        table {
-            border-spacing: 2vh 10px; /* Set spacing for both horizontial and veritcal value respectively. */
-            border-collapse: separate;            
-        }
-
-        th, td {
-            border: 5px solid darkgray; /* 5px is for test debug purpose. 2px would be better. */
-            border-radius: 15px;
-            align-items: center;
-            padding: 1rem;
-            background-color: lightcoral;
-        }
-
-        th a {
-            color: lightgreen;
-        }
-    </style>
 </head>
     <body>
-        <form action="archive.php" method="post">
-            <!-- Webpage's main title name -->
-            <div id="title">Archived Tournament</div>
-            <!-- Create a table (Like table function in Excel, MS Word, etc). To create border line, use "border" attribute -->
-            <table>
-                <!-- Create table rows, to adjust the position then use "align" attribute -->
-                <tr>
-                    <!-- Create table headers (How many vertical boxes will be needed to be create in the table row tag). To direct VOT main spreadsheets, use <a> tag -->
-                    <th><a href="https://docs.google.com/spreadsheets/d/1NTKjjdmax-qjO8L9-V8Iuom8zjcCaz9X-DHFJ4SDSAg/edit#gid=0">Vietnam osu!taiko Tournament 1</a></th>
-                    <th><a href="https://docs.google.com/spreadsheets/d/1LJg5ITi8tqUer-C2dKb1xE-MYQSELpi_-_Ur2UV_las/edit#gid=0">Vietnam osu!taiko Tournament 2</a></th>
-                    <th><a href="https://docs.google.com/spreadsheets/d/17O6J2lPWZWVvozOhT3OiumRrmFn_9wRlLQrwOHP2B-k/edit#gid=0">Vietnam osu!taiko Tournament 3</a></th>
-                    <th><a href="#">Vietnam osu!taiko Tournament 4</a></th>
-                </tr>
-            </table>
-        </form>
+        <div class="archieved-page">
+            <header>
+                <h1>Archived Tournament</h1>
+            </header>
+
+            <section>
+                <div class="flex-container">
+                    <div class="box-of-content">
+                        <a href="https://docs.google.com/spreadsheets/d/1NTKjjdmax-qjO8L9-V8Iuom8zjcCaz9X-DHFJ4SDSAg/edit#gid=0">
+                            Vietnam osu!taiko Tournament 1
+                        </a>
+                    </div>
+
+                    <div class="box-of-content">
+                        <a href="https://docs.google.com/spreadsheets/d/1LJg5ITi8tqUer-C2dKb1xE-MYQSELpi_-_Ur2UV_las/edit#gid=0">
+                            Vietnam osu!taiko Tournament 2
+                        </a>
+                    </div>
+
+                    <div class="box-of-content">
+                        <a href="https://docs.google.com/spreadsheets/d/17O6J2lPWZWVvozOhT3OiumRrmFn_9wRlLQrwOHP2B-k/edit#gid=0">
+                            Vietnam osu!taiko Tournament 3
+                        </a>
+                    </div>
+
+                    <div class="box-of-content">
+                        <p>Vietnam osu!taiko Tournament 4 (COMING SOON)</p>
+                    </div>
+
+                    <div class="box-of-content">
+                        <p>Vietnam osu!taiko Tournament 5 (COMING SOON)</p>
+                    </div>
+                    
+                    <div class="box-of-content">
+                        <p>Vietnam osu!taiko Tournament 6 (COMING SOON)</p>
+                    </div>
+                </div>
+            </section>
+        </div>
     </body>
 </html>
