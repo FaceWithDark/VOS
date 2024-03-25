50afe7af7361b8bbd2f89b5622240fcc4d6b4b4f
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2024-03-26 09:43:01 +1100

    Apply a general colour palette for the server.
    Tag adjusting in 'home' page for better maintainability.
    
    CSS styling also a bit change due to that.

--- home.php
+++ home.php
@@ -85,26 +85,26 @@
         <!-- Tournament latest news / contents. -->
         <section class="tournament-news">
             <div class="information-box" id="annoucement-box">
-                <div class="main-title">Vietnamese osu!taiko Tournament</div>
-                <p class="main-content">Welcome to the website of the biggest vietnamese osu!taiko tournament battlefield ! In here, you can look at everything related to VOT, with pages for all our current and past tournaments!</p>
+                <h2>Vietnamese osu!taiko Tournament</h2>
+                <p>Welcome to the website of the biggest vietnamese osu!taiko tournament battlefield ! In here, you can look at everything related to VOT, with pages for all our current and past tournaments!</p>
                 <!-- <div class="youtube-iframe-container">
                     <iframe src="https://www.youtube.com/embed/aeUVQe7irW4?si=HmFiaAGYbrz5fCuR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div> -->
             </div>
 
             <div class="information-box">
-                <div class="main-title">Vietnamese osu!taiko Tournament 3</div>
-                <p class="main-content">The Vietnamese osu!taiko Tournament 3 is our 3rd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
+                <h2>Vietnamese osu!taiko Tournament 3</h2>
+                <p>The Vietnamese osu!taiko Tournament 3 is our 3rd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
             </div>
 
             <div class="information-box">
-                <div class="main-title">Vietnamese osu!taiko Tournament 2</div>
-                <p class="main-content">The Vietnamese osu!taiko Tournament 2 is our 2nd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
+                <h2>Vietnamese osu!taiko Tournament 2</h2>
+                <p>The Vietnamese osu!taiko Tournament 2 is our 2nd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
             </div>
 
             <div class="information-box">
-                <div class="main-title">Vietnamese osu!taiko Tournament 1</div>
-                <p class="main-content">The Vietnamese osu!taiko Tournament 1 is our 1st vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
+                <h2>Vietnamese osu!taiko Tournament 1</h2>
+                <p>The Vietnamese osu!taiko Tournament 1 is our 1st vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
             </div>
         </section>
 
