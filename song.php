ac6bf25ac55e55aa25bb8cf89b8a7b353ddc44a0
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2024-03-28 22:59:29 +1100

    Remove some extra lines on all pages.
    - CSS file has been included through PHP calls so it is not needed to be call again in all pages.

--- song.php
+++ song.php
@@ -8,9 +8,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VOT</title>
-
-    <link rel="stylesheet" href="style.css">
-
 </head>
     <body>
         <div class="song-page">
