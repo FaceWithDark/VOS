c86b6d1cfd62b49931ef1839931496054ca60b92
FaceWithDark <binhsan1307@gmail.com>
2024-03-28 22:12:29 +1100

    More update to 'Song' page.
    - Simple box for song details are created.
    - Fix typo on the comment for 2 pages.

--- song.php
+++ song.php
@@ -17,6 +17,28 @@
             <header>
                 <h1>Banger Song 🔥</h1>
             </header>
+
+            <section>
+                <!-- TODO: 
+                        - Include song's image and other related info in.
+                        - PHP can handle this. Need to get data from osu! API only.
+                        - Direct link will be change to Soundclound link (if there is one).
+                 -->                
+                <div class="flex-container">
+                    <div class="box-of-content">
+                        <a href="https://osu.ppy.sh/beatmapsets/2032103#taiko/4235709">
+                            ILFK (SectorJack & Edolmary)
+                        </a>
+                    </div>
+
+                    <div class="box-of-content">
+                        <a href="#">Song 2</a>
+                    </div>
+
+                    <div class="box-of-content">
+                        <a href="#">Song 3</a>
+                    </div>
+            </section>
         </div>    
     </body>
 </html>
