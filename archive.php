e00148ef3c9533c20a7705faa4bb8188352c9ed5
FaceWithDark <binhsan1307@gmail.com>
2024-03-28 22:59:29 +1100

    Remove some extra lines on all pages.
    - CSS file has been included through PHP calls so it is not needed to be call again in all pages.

--- archive.php
+++ archive.php
@@ -1,5 +1,5 @@
 <?php
-    include 'navigation-bar.html';
+    include './navigation-bar.html';
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +8,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VOT</title>
-
-    <link rel="stylesheet" href="css/style.css">
-
 </head>
     <body>
         <div class="archieved-page">
