7d62c5f5905d2537a723b5836396d20dc4bade9e
FaceWithDark <binhsan1307@gmail.com>
2024-04-15 23:50:01 +1000

    Added 'reset.css' file.
    By adding this reset file, every elements will be converted to the original size, weight, height, width, etc. This will helps developer easier in scaling the UI as their preference  for that specific elements.
    
    Continue removing some unnecessary comments.
    
    Include the reset file in the 'Navigation Bar' file so it applied across all pages.
    
    Move 'Global styling' a.k.a '*' into reset file for better maintainability.

--- oauth/navigation_bar.php
+++ oauth/navigation_bar.php
@@ -57,6 +57,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VOT</title>
 
+    <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/main.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
