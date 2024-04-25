4b1e0768186742459eeecd80f2a927f5de2abe8c
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2024-04-25 11:47:24 +1000

    Merge all file into 'public' folder for production stage.
    TODO: Create a 'staging' folder later for testing phase (like an actual company-based for producting a website).

--- public/oauth/navigation_bar.php
+++ public/oauth/navigation_bar.php
@@ -9,7 +9,7 @@
     
     use Dotenv\Dotenv;
 
-    include '../vendor/autoload.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
 
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv -> load();
