ebc69d7e87ebc1d80eb048ae25c76345cc1c0659
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2024-05-09 19:31:49 +1000

    Fix <meta> tag presences in <body> tag.
    Also added more favicon images compatibility for Chrome on Android devices.

--- pages/archive.php
+++ pages/archive.php
@@ -2,52 +2,42 @@
     require_once '../oauth/navigation_bar.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VOT</title>
-</head>
-    <body>
-        <div class="archived-page">
-            <header>
-                <h1>Archived Tournament</h1>
-            </header>
+<div class="archived-page">
+    <header>
+        <h1>Archived Tournament</h1>
+    </header>
 
-            <section>
-                <div class="flex-container">
-                    <div class="direct-link-container">
-                        <a href="https://docs.google.com/spreadsheets/d/1NTKjjdmax-qjO8L9-V8Iuom8zjcCaz9X-DHFJ4SDSAg/edit#gid=0">
-                            Vietnam osu!taiko Tournament 1
-                        </a>
-                    </div>
+    <section>
+        <div class="flex-container">
+            <div class="direct-link-container">
+                <a href="https://docs.google.com/spreadsheets/d/1NTKjjdmax-qjO8L9-V8Iuom8zjcCaz9X-DHFJ4SDSAg/edit#gid=0">
+                    Vietnam osu!taiko Tournament 1
+                </a>
+            </div>
 
-                    <div class="direct-link-container">
-                        <a href="https://docs.google.com/spreadsheets/d/1LJg5ITi8tqUer-C2dKb1xE-MYQSELpi_-_Ur2UV_las/edit#gid=0">
-                            Vietnam osu!taiko Tournament 2
-                        </a>
-                    </div>
+            <div class="direct-link-container">
+                <a href="https://docs.google.com/spreadsheets/d/1LJg5ITi8tqUer-C2dKb1xE-MYQSELpi_-_Ur2UV_las/edit#gid=0">
+                    Vietnam osu!taiko Tournament 2
+                </a>
+            </div>
 
-                    <div class="direct-link-container">
-                        <a href="https://docs.google.com/spreadsheets/d/17O6J2lPWZWVvozOhT3OiumRrmFn_9wRlLQrwOHP2B-k/edit#gid=0">
-                            Vietnam osu!taiko Tournament 3
-                        </a>
-                    </div>
+            <div class="direct-link-container">
+                <a href="https://docs.google.com/spreadsheets/d/17O6J2lPWZWVvozOhT3OiumRrmFn_9wRlLQrwOHP2B-k/edit#gid=0">
+                    Vietnam osu!taiko Tournament 3
+                </a>
+            </div>
 
-                    <div class="direct-link-container">
-                        <a>Vietnam osu!taiko Tournament 4 (COMING SOON)</a>
-                    </div>
+            <div class="direct-link-container">
+                <a>Vietnam osu!taiko Tournament 4 (COMING SOON)</a>
+            </div>
 
-                    <div class="direct-link-container">
-                        <a>Vietnam osu!taiko Tournament 5 (COMING SOON)</a>
-                    </div>
-                    
-                    <div class="direct-link-container">
-                        <a>Vietnam osu!taiko Tournament 6 (COMING SOON)</a>
-                    </div>
-                </div>
-            </section>
+            <div class="direct-link-container">
+                <a>Vietnam osu!taiko Tournament 5 (COMING SOON)</a>
+            </div>
+            
+            <div class="direct-link-container">
+                <a>Vietnam osu!taiko Tournament 6 (COMING SOON)</a>
+            </div>
         </div>
-    </body>
-</html>
+    </section>
+</div>
--- pages/index.php
+++ pages/index.php
@@ -2,37 +2,27 @@
     require_once '../oauth/navigation_bar.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VOT</title>
-</head>
-    <body>
-        <section class="tournament-news">
-            <div class="announcement-box" id="latest-news">
-                <h2>Vietnamese osu!taiko Tournament</h2>
-                <p>Welcome to the website of the biggest vietnamese osu!taiko tournament battlefield ! In here, you can look at everything related to VOT, with pages for all our current and past tournaments!</p>
-                <div class="youtube-iframe-container">
-                    <iframe src="https://www.youtube.com/embed/aeUVQe7irW4?si=HmFiaAGYbrz5fCuR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </div>
-            </div>
+<section class="tournament-news">
+    <div class="announcement-box" id="latest-news">
+        <h2>Vietnamese osu!taiko Tournament</h2>
+        <p>Welcome to the website of the biggest vietnamese osu!taiko tournament battlefield ! In here, you can look at everything related to VOT, with pages for all our current and past tournaments!</p>
+        <div class="youtube-iframe-container">
+            <iframe src="https://www.youtube.com/embed/aeUVQe7irW4?si=HmFiaAGYbrz5fCuR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    </div>
 
-            <div class="announcement-box">
-                <h2>Vietnamese osu!taiko Tournament 3</h2>
-                <p>The Vietnamese osu!taiko Tournament 3 is our 3rd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
-            </div>
+    <div class="announcement-box">
+        <h2>Vietnamese osu!taiko Tournament 3</h2>
+        <p>The Vietnamese osu!taiko Tournament 3 is our 3rd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
+    </div>
 
-            <div class="announcement-box">
-                <h2>Vietnamese osu!taiko Tournament 2</h2>
-                <p>The Vietnamese osu!taiko Tournament 2 is our 2nd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
-            </div>
+    <div class="announcement-box">
+        <h2>Vietnamese osu!taiko Tournament 2</h2>
+        <p>The Vietnamese osu!taiko Tournament 2 is our 2nd vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
+    </div>
 
-            <div class="announcement-box">
-                <h2>Vietnamese osu!taiko Tournament 1</h2>
-                <p>The Vietnamese osu!taiko Tournament 1 is our 1st vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
-            </div>
-        </section>
-    </body>
-</html>
+    <div class="announcement-box">
+        <h2>Vietnamese osu!taiko Tournament 1</h2>
+        <p>The Vietnamese osu!taiko Tournament 1 is our 1st vietnamese-based osu!taiko tournament targeting a much larger players scale, from inf - #5000 !</p>
+    </div>
+</section>
