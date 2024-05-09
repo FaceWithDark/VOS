87c09caa73c1ddfee4fa9ec88ec67270ea17935d
FaceWithDark <binhsan1307@gmail.com>
2024-05-09 19:31:49 +1000

    Fix <meta> tag presences in <body> tag.
    Also added more favicon images compatibility for Chrome on Android devices.

--- pages/song.php
+++ pages/song.php
@@ -2,40 +2,30 @@
     require_once '../oauth/navigation_bar.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VOT</title>
-</head>
-    <body>
-        <div class="song-page">
-            <header>
-                <h1>Banger Song 🔥</h1>
-            </header>
+<div class="song-page">
+    <header>
+        <h1>Banger Song 🔥</h1>
+    </header>
 
-            <section>
-                <!-- TODO: 
-                        - Include song's image and other related info in.
-                        - PHP can handle this. Need to get data from osu! API only.
-                        - Direct link will be change to Soundclound link (if there is one).
-                 -->                
-                <div class="flex-container">
-                    <div class="direct-link-container">
-                        <a href="https://osu.ppy.sh/beatmapsets/2032103#taiko/4235709">
-                            ILFK (SectorJack & Edolmary)
-                        </a>
-                    </div>
+    <section>
+        <!-- TODO: 
+                - Include song's image and other related info in.
+                - PHP can handle this. Need to get data from osu! API only.
+                - Direct link will be change to Soundclound link (if there is one).
+            -->                
+        <div class="flex-container">
+            <div class="direct-link-container">
+                <a href="https://osu.ppy.sh/beatmapsets/2032103#taiko/4235709">
+                    ILFK (SectorJack & Edolmary)
+                </a>
+            </div>
 
-                    <div class="direct-link-container">
-                        <a href="#">Song 2</a>
-                    </div>
+            <div class="direct-link-container">
+                <a href="#">Song 2</a>
+            </div>
 
-                    <div class="direct-link-container">
-                        <a href="#">Song 3</a>
-                    </div>
-            </section>
-        </div>    
-    </body>
-</html>
+            <div class="direct-link-container">
+                <a href="#">Song 3</a>
+            </div>
+    </section>
+</div>
--- pages/staff.php
+++ pages/staff.php
@@ -2,43 +2,33 @@
     require_once '../oauth/navigation_bar.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VOT</title>
-</head>
-    <body>
-        <div class="staff-page">
-            <header>
-                <h1>Our Lovely Staff 💖</h1>
-            </header>
+<div class="staff-page">
+    <header>
+        <h1>Our Lovely Staff 💖</h1>
+    </header>
 
-            <section>
-                <!-- TODO: 
-                        - Include avatar and other related info in.
-                        - PHP can handle this. Need to get data from osu! API only.
-                 -->
-                <div class="flex-container">
-                    <div class="direct-link-container">
-                        <a href="https://osu.ppy.sh/users/9623142">
-                            ダビッド
-                        </a>
-                    </div>
+    <section>
+        <!-- TODO: 
+                - Include avatar and other related info in.
+                - PHP can handle this. Need to get data from osu! API only.
+            -->
+        <div class="flex-container">
+            <div class="direct-link-container">
+                <a href="https://osu.ppy.sh/users/9623142">
+                    ダビッド
+                </a>
+            </div>
 
-                    <div class="direct-link-container">
-                        <a href="https://osu.ppy.sh/users/16688499">
-                            Nj! Scroll God
-                        </a>
-                    </div>
+            <div class="direct-link-container">
+                <a href="https://osu.ppy.sh/users/16688499">
+                    Nj! Scroll God
+                </a>
+            </div>
 
-                    <div class="direct-link-container">
-                        <a href="https://osu.ppy.sh/users/16039831">
-                            Aki Blind God
-                        </a>
-                    </div>
-            </section>
-        </div>    
-    </body>
-</html>
+            <div class="direct-link-container">
+                <a href="https://osu.ppy.sh/users/16039831">
+                    Aki Blind God
+                </a>
+            </div>
+    </section>
+</div>
