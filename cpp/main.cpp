c72415760fdc45a0b0567a7e9f96e24f4e0f3020
FaceWithDark <binhsan1307@gmail.com>
2024-07-25 20:33:37 +1000

    feat(chatbot): enable dynamic user input
    - Updated main function file to capturing user input from the console.
    - This change allows users to type in their queries freely, making the chatbot more interactive and user-friendly.
    - Ensured the chatbot processes the entire line of input and generates appropriate responses based on predefined keywords.
    
    This commit enhances the chatbot's interactivity by allowing it to handle dynamic user input directly from the console.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -11,8 +11,17 @@ int main()
         std::string userInput = "How do I get to the home page?";
         std::string userInput = "what does dark / light mode do?";
         std::string userInput = "I am having issue with my account.";
+        std::string userInput = "Goodbye, VOT Copilot!";   
     */
-    std::string userInput = "Goodbye, VOT Copilot!";   
+
+    // Store user input as string
+    std::string userInput;
+
+    // Prompt user to type in for further use
+    std::cout << "Query: ";
+
+    // Get user input and sent to chatbot to analyse it
+    std::getline(std::cin, userInput);
 
     // Get the chatbot's response
     std::string chatbotResponse = votchatbot.getResponse(userInput);
