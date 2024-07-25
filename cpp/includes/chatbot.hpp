2c52397ab564731c167d332377f0f6e4c9ef958c
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2024-07-25 16:46:21 +1000

    feat(chatbot): add global 'Chatbot' class with greeting functionality
    BREAKING CHANGES: this is the new format for implementing dynamic chatbot interaction with user using a global class to easily tweak or adjust (if needed) in the future. For any developers that follow on with this project, please refers to this commit from now on as the initial commit for implementating C++ chatbot with PHP website.
    
    - Implemented a global-use class to handle user input and generate responses.
    - Added greeting keywords and corresponding responses.
    - Included a method to convert uppercase strings to lowercase strings for case-insensitive matching.
    - Added logic to handle 'non-matching' scenario with a default message.
    - Provided a test case in main function to test the chatbot with sample user input
    
    This commit sets up a basic structure for the chatbot, allowing for easy expansion with additional keywords and responses.

--- cpp/includes/chatbot.hpp
+++ cpp/includes/chatbot.hpp
@@ -1,29 +1,23 @@
-#if !defined(CHATBOT_HPP)
-#define CHATBOT_HPP
+#if !defined(VOTCHATBOT_HPP)
+#define VOTCHATBOT_HPP
 
 #include <string>
 #include <unordered_map>
-#include <vector>
 
 class VOTChatbot {
 public:
-
+    // Constructor to initialise the chatbot
     VOTChatbot();
-    // Output the defined queries that has been prepared to get display out to the terminal
-    std::string respondQuery(const std::string& query);
+
+    // Get the chatbot's response based on user input
+    std::string getResponse(const std::string& userInput);
 
 private:
-    // Process the defined queries to get ready to be ouput to the terminal 
-    std::string processQuery(const std::string& query);
+    // Convert any uppercase string to lowercase string to support both typing format
+    std::string upperToLowerCase(const std::string& keywordString);
 
-    // Data member for storing predefined responses
-    std::unordered_map<std::string, std::string> predefinedResponses;
-
-    std::vector<std::string> greetingQuery;     // Query for greeting keywords
-    std::vector<std::string> directionQuery;    // Query for direction keywords
-
-    // Check if the queries match any string from the string provided into queries vectors
-    bool isQueryMatch(const std::vector<std::string>& queries, const std::string& query);
+    // Pre-defined keywords and responses
+    std::unordered_map<std::string, std::string> greetingKeywords;
 };
 
 #endif
