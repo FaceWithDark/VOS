9e2cb7e239b7313c9e69b03a921ca7435f801c39
FaceWithDark <binhsan1307@gmail.com>
2024-07-27 11:17:40 +1000

    Start to work on refactor 'song' page
    Inspired from what I have done with 'mappool' page, I am doing a fairly similar thing to this page. Currently:
    
    - Create some example format for displaying custom song data that 90% similar to how I did with beatmap data.
    - Update CSS file according to the example format as well.

--- pages/song.php
+++ pages/song.php
@@ -2,30 +2,87 @@
     require_once '../layouts/navigation_bar.php';
 ?>
 
-<div class="song-page">
-    <header>
-        <h1>Banger Song 🔥</h1>
-    </header>
+<section>
+    <div class="header-container">
+        <h1>Banger Song <i class='bx bxs-hot'></i></h1>
+    </div>
 
-    <section>
+    <div class="song-page">
         <!-- TODO: 
-                - Include song's image and other related info in.
-                - PHP can handle this. Need to get data from osu! API only.
-                - Direct link will be change to Soundclound link (if there is one).
-            -->                
-        <div class="flex-container">
-            <div class="direct-link-container">
-                <a href="https://osu.ppy.sh/beatmapsets/2032103#taiko/4235709">
-                    ILFK (SectorJack & Edolmary)
-                </a>
+            - Include song's image and other related info in.
+            - PHP can handle this. Need to get data from osu! API only.
+            - Direct link will be change to Soundclound link (if there is one).
+        -->                
+        <div class="song-card-container">
+            <h1>VOT3 - Quarterfinals - HD2</h1>
+            <br>
+            <a href="https://osu.ppy.sh/beatmapsets/2032103#taiko/4235709"><img src="" width="490px" alt="Beatmap Cover"></a>
+            <br><br>
+            <h2>ILFK [davidminh & njshift's Terrestrial Oni]</h2>
+            <h3>SectorJack & Edolmary</h3>
+            <h4 class="beatmap-creator-row">
+                Mapset by <a href="https://osu.ppy.sh/users/9623142">davidminh0111</a>
+            </h4>
+            <br>
+            <div class="beatmap-attribute-row">
+                <p style="margin-right: 1rem;"><i class='bx bx-star'></i>abc</p>
+                <p style="margin-right: 1rem;"><i class='bx bx-timer'></i>abc</p>
+                <p><i class='bx bx-tachometer'></i>abc bpm</p>
             </div>
+            <br>
+            <div class="beatmap-attribute-row">
+                <p style="margin-right: 1rem;">OD: abc</p>
+                <p style="margin-right: 1rem;">HP: abc</p>
+                <p>Passed: abc</p>
+            </div>
+        </div>
 
-            <div class="direct-link-container">
-                <a href="#">Song 2</a>
+        <div class="song-card-container">
+            <h1>abc</h1>
+            <br>
+            <a href="#"><img src="" width="490px" alt="Beatmap Cover"></a>
+            <br><br>
+            <h2>abc</h2>
+            <h3>abc</h3>
+            <h4 class="beatmap-creator-row">
+                Mapset by <a href="#">abc</a>
+            </h4>
+            <br>
+            <div class="beatmap-attribute-row">
+                <p style="margin-right: 1rem;"><i class='bx bx-star'></i>abc</p>
+                <p style="margin-right: 1rem;"><i class='bx bx-timer'></i>abc</p>
+                <p><i class='bx bx-tachometer'></i>abc bpm</p>
             </div>
+            <br>
+            <div class="beatmap-attribute-row">
+                <p style="margin-right: 1rem;">OD: abc</p>
+                <p style="margin-right: 1rem;">HP: abc</p>
+                <p>Passed: abc</p>
+            </div>
+        </div>
 
-            <div class="direct-link-container">
-                <a href="#">Song 3</a>
+        <div class="song-card-container">
+            <h1>abc</h1>
+            <br>
+            <a href="#"><img src="" width="490px" alt="Beatmap Cover"></a>
+            <br><br>
+            <h2>abc</h2>
+            <h3>abc</h3>
+            <h4 class="beatmap-creator-row">
+                Mapset by <a href="#">abc</a>
+            </h4>
+            <br>
+            <div class="beatmap-attribute-row">
+                <p style="margin-right: 1rem;"><i class='bx bx-star'></i>abc</p>
+                <p style="margin-right: 1rem;"><i class='bx bx-timer'></i>abc</p>
+                <p><i class='bx bx-tachometer'></i>abc bpm</p>
             </div>
-    </section>
-</div>
+            <br>
+            <div class="beatmap-attribute-row">
+                <p style="margin-right: 1rem;">OD: abc</p>
+                <p style="margin-right: 1rem;">HP: abc</p>
+                <p>Passed: abc</p>
+            </div>
+        </div>
+    </div>
+</section>
