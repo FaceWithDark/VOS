95abfce11a0fc454661a43294f024f4cd8e87e8f
FaceWithDark <binhsan1307@gmail.com>
2024-07-27 13:30:37 +1000

    Add function to fetch custom song data from API
    - With debugging codes as well for future rollback if anything happens unexpectedly.

--- pages/song.php
+++ pages/song.php
@@ -1,5 +1,53 @@
 <?php
-    require_once '../layouts/navigation_bar.php';
+
+declare(strict_types = 1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+require_once '../layouts/navigation_bar.php';
+include_once '../modules/convertion/time_convertion.php';
+// require '../layouts/configuration.php';
+
+// Fetch custom song data from the Osu! API
+function fetchCustomSongData() {
+    // Check if the user is authenticated by looking for the access token in cookies
+    $accessToken = $_COOKIE['vot_access_token'] ?? null;
+    if ($accessToken) {
+        // If authenticated, construct the API URL for fetching beatmap data
+        $apiUrl = "https://osu.ppy.sh/api/v2/beatmaps/4235709";
+        $client = new Client();
+    
+        try {
+            // Make a GET request to the Osu! API with the access token
+            $response = $client -> get($apiUrl, [
+                'headers' => [
+                    'Authorization' => "Bearer {$accessToken}",
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                ]
+            ]);
+            
+            // Return the beatmap data if it is a 200 status 
+            if ($response->getStatusCode() === 200) {
+                $apiData = json_decode($response -> getBody() -> getContents());
+                return $apiData;
+            }
+            // API call did not return a 200 status
+            return false;
+        } 
+        catch (RequestException $exception) {
+            error_log("API request failed: " . $exception -> getMessage());  // Log the exception message
+            return false;                                                    // An exception occurred during the API call
+        }
+    }
+}
+
+$customSongData = fetchCustomSongData();
+die('<pre>' . print_r($customSongData, true) . '</pre>');
+
 ?>
 
 <section>
@@ -14,7 +62,7 @@
             - Direct link will be change to Soundclound link (if there is one).
         -->                
         <div class="song-card-container">
-            <h1>VOT3 - Quarterfinals - HD2</h1>
+            <h1>VOT3 - Semifinals - HD1</h1>
             <br>
             <a href="https://osu.ppy.sh/beatmapsets/2032103#taiko/4235709"><img src="" width="490px" alt="Beatmap Cover"></a>
             <br><br>
