e256b050d56c0be558a58a71225ccbdc8addd775
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2024-10-19 15:07:56 +1100

    Synchronise coding method in 'Archive' page
    - Basically, talking like a normal human-being, I just adjust the current format for making tournament buttons into the same format like VOT3 and VOT4. However, as we haven't still done recovering the data for other VOTs yet so those two changed format's buttons will still be locked until further notice.

--- pages/archive.php
+++ pages/archive.php
@@ -10,18 +10,17 @@ require_once '../layouts/navigation_bar.php';
     <section>
         <form action="mappool.php" method="get">
             <div class="flex-container">
+
                 <div class="direct-link-container">
                     <!-- <a href="https://docs.google.com/spreadsheets/d/1NTKjjdmax-qjO8L9-V8Iuom8zjcCaz9X-DHFJ4SDSAg/edit#gid=0"> // TODO: Re-create this as the spreadsheet owner somehow delete this. -->
-                    <a href="#">
-                        Vietnam osu!taiko Tournament 1
-                    </a>
+                    <!-- <button type="submit" name="name" value="vot1">Vietnam osu!taiko Tournament 1</button> -->
+                    <a href="#">Vietnam osu!taiko Tournament 1</a>
                 </div>
 
                 <div class="direct-link-container">
                     <!-- <a href="https://docs.google.com/spreadsheets/d/1LJg5ITi8tqUer-C2dKb1xE-MYQSELpi_-_Ur2UV_las/edit#gid=0"> // TODO: Re-create this as the spreadsheet owner somehow delete this. -->
-                    <a href="#">
-                        Vietnam osu!taiko Tournament 2
-                    </a>
+                    <!-- <button type="submit" name="name" value="vot2">Vietnam osu!taiko Tournament 2</button> -->
+                    <a href="#">Vietnam osu!taiko Tournament 2</a>
                 </div>
 
                 <div class="direct-link-container">
@@ -32,14 +31,6 @@ require_once '../layouts/navigation_bar.php';
                 <div class="direct-link-container">
                     <!-- <a href="https://docs.google.com/spreadsheets/d/1H9N4hm1gcnipTtzKNtGpana7mJ2Gh4EbTnq9B19pCtU/edit?gid=1461400646#gid=1461400646"> -->
                     <button type="submit" name="name" value="vot4">Vietnam osu!taiko Tournament 4</button>
-                </div>
-
-                <div class="direct-link-container">
-                    <a>Vietnam osu!taiko Tournament 5 (COMING SOON)</a>
-                </div>
-
-                <div class="direct-link-container">
-                    <a>Vietnam osu!taiko Tournament 6 (COMING SOON)</a>
                 </div>
             </div>
         </form>
