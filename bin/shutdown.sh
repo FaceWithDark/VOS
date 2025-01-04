5ea25cd163617819c74e01a29ff10314cd70e3a9
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2025-01-05 01:57:56 +1100

    Update automate build script
    - Adjust ignored files in .gitignore to match with my preferred file
      naming convention for this project.
    
    - As the result of this, all build scripts needed to be adjust to match
      with the new change.

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -4,27 +4,34 @@
 ENV=$1
 
 if [ -z "$ENV" ]; then
-  echo "Please use one of the following accepted environment names: 'dev', 'stage', and 'prod'!"
+  echo "Please use one of the following accepted build phases: 'dev', 'stage', and 'prod'!"
   exit 1
 fi
 
-# Define the docker-compose.env file based on the environment
-ENV_FILE="../docker-compose.env-$ENV"
+# Define Docker-needed configuration files based on the environment
+ENV_FILE="../docker-compose.$ENV.env"
+YAML_FILE="../docker-compose.$ENV.yaml"
 
-# Check if the docker-compose.env file exists
+# Make sure the script found .env file based on the environment
 if [ ! -f "$ENV_FILE" ]; then
   echo "Requested environment file: $ENV_FILE not found! Consider create one on your own using the same file format and try again."
   exit 1
 fi
 
-# Load the environment variables from the docker-compose.env file
+# Make sure the script found .yaml file based on the environment
+if [ ! -f "$YAML_FILE" ]; then
+  echo "Requested YAML file: $YAML_FILE not found! Consider create one on your own using the same file format and try again."
+  exit 1
+fi
+
+# Load needed variables from the environment file based on the environment
 export $(grep -v '^#' "$ENV_FILE" | xargs)
 
 # Validate required environment variables
 ENV_VARS=("DATABASE_ROOT_PASSWORD" "DATABASE_NAME" "DATABASE_USER" "DATABASE_PASSWORD")
 for env_var in "${ENV_VARS[@]}"; do
   if [ -z "${!env_var}" ]; then
-    echo "Required environment variable: $var is not set!"
+    echo "Required environment variable: '$env_var' is not set!"
     exit 1
   fi
 done
@@ -33,39 +40,39 @@ done
 export ENV
 
 # Shutdown Docker Compose
-echo "Shutting down Docker for '$ENV' stage..."
-docker-compose down
+echo "Shutting down Docker for '$ENV' phase..."
+docker-compose -f "$YAML_FILE" down
 
 # Clean up unused Docker images
 echo "Removing unused Docker images..."
-docker image prune -f
+docker image prune --force
 
 # Clean up unused Docker volumes
 echo "Removing unused Docker volumes..."
-docker volume prune -f
+docker volume prune --force
 
 # Clean up unused Docker networks
 echo "Removing unused Docker networks..."
-docker network prune -f
+docker network prune --force
 
 # Clean up unused Docker build cache
 echo "Removing unused Docker build cache..."
-docker builder prune -f
+docker builder prune --force
 
 # Clean up all unused Docker objects (images, containers, volumes, and networks)
 echo "Removing all unused Docker objects..."
-docker system prune -af
+docker system prune -a --force
 
 # Remove all stopped containers
 echo "Removing all stopped containers..."
-docker container prune -f
+docker container prune --force
 
 # Remove all dangling images
 echo "Removing all dangling images..."
-docker image prune -f --filter "dangling=true"
+docker image prune --force --filter "dangling=true"
 
 # Remove all unused local volumes not used by at least one container
 echo "Removing all unused local volumes..."
-docker volume prune -f
+docker volume prune --force
 
-echo "Docker session for stage '$ENV' has been shut down and cleaned!"
+echo "Docker session for phase '$ENV' has been shut down and cleaned!"
