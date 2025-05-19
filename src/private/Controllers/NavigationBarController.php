242f05c0899653edf256ed495d41c2386eea6e4a
FaceWithDark <114546939+FaceWithDark@users.noreply.github.com>
2025-05-19 22:34:41 +1000

    Re-write MVC system design structure (Part 5)
    - Quick summary: I moved all logics that use the user access token and
      temporary code into its own dedicated page and function methods/files.
      This is so that each feature only being used for one purpose only.
      Also, I think that having a deidicated folder for non-MVC structure
      would be a better approach than using a general `Controller.php` for
      that particular purposes.

--- src/private/Controllers/NavigationBarController.php
+++ src/private/Controllers/NavigationBarController.php
@@ -2,8 +2,7 @@
 # Not so much like static types, but at least it does feel better having this here
 declare(strict_types=1);
 
-require __DIR__ . '/../Controllers/Controller.php';
-require __DIR__ . '/../Models/NavigationBar.php';
+require __DIR__ . '/../Configurations/Environment.php';
 
 function getUserAuthoriseCode(): null
 {
@@ -136,76 +135,3 @@ function getUserAccessToken(string $temporary_code): bool | null
         }
     }
 }
-
-/*
-function getUserData(string $access_token): array | bool
-{
-    $osuUserUrl                 = "https://osu.ppy.sh/api/v2/me/taiko";
-    $osuUserAccessToken         = $_COOKIE['vot_access_token'];
-
-    $httpAcceptType                 = 'application/json';
-    $httpContentType                = 'application/json';
-
-    if (version_compare(PHP_VERSION, '5.4.0', '>=')) {
-        $httpHeaderRequest = [
-            "Accept: $httpAcceptType",
-            "Content-Type: $httpContentType",
-        ];
-    } else {
-        $httpHeaderRequest = array(
-            "Accept: $httpAcceptType",
-            "Content-Type: $httpContentType",
-        );
-    }
-
-    # CURL session will be handled manually through curl_setopt()
-    $curlHandle = curl_init(url: null);
-
-    curl_setopt(handle: $curlHandle, option: CURLOPT_URL, value: $userAccessTokenUrl);
-    curl_setopt(handle: $curlHandle, option: CURLOPT_HTTPHEADER, value: $httpHeaderRequest);
-    curl_setopt(handle: $curlHandle, option: CURLOPT_HEADER, value: 0);
-    curl_setopt(handle: $curlHandle, option: CURLOPT_RETURNTRANSFER, value: 1);
-
-    $curlResponse = curl_exec(handle: $curlHandle);
-
-    if (curl_errno(handle: $curlHandle)) {
-        error_log(curl_error(handle: $curlHandle));
-        curl_close(handle: $curlHandle);
-        return false; // An error occurred during the API call
-
-    } else {
-        $curlDecodedResponse = json_decode(
-            json: $curlResponse,
-            associative: true,
-            depth: 512,
-            flags: 0
-        );
-}
-
-// Exchange the authorisation code for an access token
-$userData = getUserData(token: $osuUserAccessToken, url: $osuUserUrl);
-
-if ($userData !== false) {
-    // echo ('User data received');
-    $userId         = $userData->id;
-    $userImage      = $userData->avatar_url;
-    $userUrl        = $userData->user_url; // TODO: no built-in API for this. Create one.
-    $userFlag       = $userData->country_code;
-    $userName       = $userData->username;
-    $userRank       = $userData->statistics->global_rank;
-    $userTimeZone   = $userData->$user_timezone; // TODO: no built-in API for this. Create one.
-
-    createUserData(
-        id: $userId,
-        image: $userImage,
-        url: $userUrl,
-        flag: $userFlag,
-        name: $userName,
-        rank: $userRank,
-        time_zone: $userTimeZone,
-        database_connection: $userData # TODO: move database connection code into a general include-able file (maybe, `Controller.php`)
-    );
-} else {
-    die();
-}
-*/
--- src/private/Views/NavigationBar/AuthorisedNavigationBarView.php
+++ src/private/Views/NavigationBar/AuthorisedNavigationBarView.php
@@ -1,3 +1,5 @@
+<?php require __DIR__ . '/../../Models/NavigationBar.php'; ?>
+
 <!-- XHTML 1.0 compatible -->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
