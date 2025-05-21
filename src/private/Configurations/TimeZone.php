212080f493c284694b10e8c6ba125e05680ed2cc
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2025-05-22 00:01:03 +1000

    Re-write MVC system design structure (Part 6)
    - Quick summary: this commit is just me moving logic/files around a bit
      more to fix the warning on 'Home' page when user authorised. It is all
      fixed now, after 3 commits. I did think again about showing the access
      token and decided not to do that due to unesscessary techinical
      complexity added into the website. Also, I moved helper functions into
      its own file to serve its purpose.

---
+++ src/private/Configurations/TimeZone.php
@@ -0,0 +1,55 @@
+<?php
+# Not so much like static types, but at least it does feel better having this here
+declare(strict_types=1);
+
+require __DIR__ . '/InternetProtocol.php';
+require __DIR__ . '/Environment.php';
+
+function getUserTimeZone(): array | bool
+{
+    $osuUserIp              = getUserIp()['ip'];
+    $osuUserTimeZoneApiKey  = getenv(name: 'IPIFY_API_KEY', local_only: true) ?: getenv(name: 'IPIFY_API_KEY');
+    $osuUserTimeZoneUrl     = "https://geo.ipify.org/api/v2/country?apiKey=$osuUserTimeZoneApiKey&ipAddress=$osuUserIp";
+
+    $httpAcceptType         = 'application/json';
+    $httpContentType        = 'application/json';
+
+    if (version_compare(PHP_VERSION, '5.4.0', '>=')) {
+        $httpHeaderRequest = [
+            "Accept: $httpAcceptType",
+            "Content-Type: $httpContentType",
+        ];
+    } else {
+        $httpHeaderRequest = array(
+            "Accept: $httpAcceptType",
+            "Content-Type: $httpContentType",
+        );
+    }
+
+    # CURL session will be handled manually through curl_setopt()
+    $userTimeZoneCurlHandle = curl_init(url: null);
+
+    curl_setopt(handle: $userTimeZoneCurlHandle, option: CURLOPT_URL, value: $osuUserTimeZoneUrl);
+    curl_setopt(handle: $userTimeZoneCurlHandle, option: CURLOPT_HTTPHEADER, value: $httpHeaderRequest);
+    curl_setopt(handle: $userTimeZoneCurlHandle, option: CURLOPT_HEADER, value: 0);
+    curl_setopt(handle: $userTimeZoneCurlHandle, option: CURLOPT_RETURNTRANSFER, value: 1);
+
+    $userTimeZoneCurlResponse = curl_exec(handle: $userTimeZoneCurlHandle);
+
+    if (curl_errno(handle: $userTimeZoneCurlHandle)) {
+        error_log(curl_error(handle: $userTimeZoneCurlHandle));
+        curl_close(handle: $userTimeZoneCurlHandle);
+        return false; // An error occurred during the API call
+
+    } else {
+        $userTimeZoneCurlDecodedResponse = json_decode(
+            json: $userTimeZoneCurlResponse,
+            associative: true,
+            depth: 512,
+            flags: 0
+        );
+
+        curl_close(handle: $userTimeZoneCurlHandle);
+        return $userTimeZoneCurlDecodedResponse;
+    }
+}
