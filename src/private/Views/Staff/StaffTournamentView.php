6b6dcdab9832482795f81c7d826848fc8a2c9a0b
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2025-05-24 23:12:24 +1000

    Re-write MVC system design structure (Part 8a)
    - Quick summary: Lots of TODOs and template HTML pages.

--- src/private/Views/Staff/StaffTournamentView.php
+++ src/private/Views/Staff/StaffTournamentView.php
@@ -3,11 +3,8 @@
 </header>
 
 <section class="staff-page">
-    <form action="/staff" method="get">
-        <!-- TODO: Kill the page with HTTP status code immediately if no data found from the database (SQL injection, maybe). -->
-        <div class="vot-center-button-container">
-            <button type="submit" name="tournament" value="VOT">VOT</button>
-            <button type="submit" name="tournament" value="VTC">VTC</button>
-        </div>
-    </form>
+    <div class="vot-center-button-container">
+        <a href="/vot">VOT</a>
+        <a href="/vtc">VTC</a>
+    </div>
 </section>
