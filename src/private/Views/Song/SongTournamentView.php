6fb9102138996a3633323096f506c2706a966b34
FaceWithDark <114546939+FaceWithDark@users.noreply.github.com>
2025-07-16 01:16:49 +1000

    Re-write MVC system design structure (Part 17)
    - Quick summary: apply UX/UI design to the displayed information about
      custom song for each tournament (if any).

--- src/private/Views/Song/SongTournamentView.php
+++ src/private/Views/Song/SongTournamentView.php
@@ -2,7 +2,7 @@
     <h1>Our Banger Song &#128293</h1>
 </header>
 
-<section class="song-page">
+<section class="custom-song-page">
     <div class="vot-center-button-container">
         <a href="/song/vot">VOT</a>
         <a href="/song/vtc">VTC</a>
