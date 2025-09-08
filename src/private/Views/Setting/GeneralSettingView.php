d13cde161683c090ecf2ad4bde8fbb82a05b8338
FaceWithDark <114546939+FaceWithDark@users.noreply.github.com>
2025-09-08 10:42:37 +1000

    Add setting page template for each roles

---
+++ src/private/Views/Setting/GeneralSettingView.php
@@ -0,0 +1,12 @@
+<header>
+    <h1>Setting</h1>
+</header>
+
+<section class="setting-page">
+    <div class="vot-center-button-container">
+        <!-- TODO: access filter to show these to staff (or admin) only, user just see the regular setting panel -->
+        <a href="/setting/admin">Admin Setting</a>
+        <a href="/setting/staff">Staff Setting</a>
+        <a href="/setting/user">User Setting</a>
+    </div>
+</section>
---
+++ src/private/Views/Setting/UserSettingView.php
@@ -0,0 +1,7 @@
+<header>
+    <h1>User Setting</h1>
+</header>
+
+<section class="user-setting-page">
+    <h1>COMING SOON!! (USER)</h1>
+</section>
