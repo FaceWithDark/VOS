a028fc91e8116ba292581a4bb15ffc1176592178
FaceWithDark <114546939+FaceWithDark@users.noreply.github.com>
2025-09-09 23:59:39 +1000

    Update automation scripts for up-to-date with new setup guide
    - Adjust one example file a little bit as I haven't found the suitable
      method for the usage of ENV variable in that example file, YET.
    
    - Remove the use of fully shutdown and remove docker containers as I
      don't think it's that needed to do like so. I can just manually remove
      if I have to.
    
    - Add a new script for quickly access the database without having to
      write a fairly long ass MariaDB command just to connect the database

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,36 +1,55 @@
 #!/bin/bash
 
-BUILD_STAGE=$1
+BUILD_STAGE=${1,,}
 
-ENV_FILE="docker.$BUILD_STAGE.env"
+if [ -z "$BUILD_STAGE" ]; then
+    echo 'Invalid build stage provided, please use one of the following option: [dev], [stage], [prod]!!'
+    exit 1
+fi
+
+case $BUILD_STAGE in
+    dev|stage|prod)
+        echo "[$BUILD_STAGE] build stage found! Move on to the next step..."
+        ;;
+    *)
+        echo 'Invalid build stage provided, please use one of the following option: [dev], [stage], [prod]!!'
+        exit 1
+        ;;
+esac
+
+
 YAML_FILE="docker-compose.$BUILD_STAGE.yaml"
 
-# Make sure all minimum required files are found
-if [ -z "$BUILD_STAGE" ]; then
-    echo "Invalid build stage found! Please use one of the following build stage: <dev>, <stage>, and <prod>"
+if [ ! -f "$YAML_FILE" ]; then
+    echo "Missing <$YAML_FILE> file!! Please double check with the setup guide (SETUP.md) again."
     exit 1
 else
-  echo "Valid build stage found! Process to the next step..."
+    echo "Found <$YAML_FILE> file!! Move on to the next step..."
 fi
+
+
+ENV_FILE="docker.$BUILD_STAGE.env"
 
 if [ ! -f "$ENV_FILE" ]; then
-  echo "Invalid .env file found! Consider create one on your own based on the repository's file format and try again."
-  exit 1
+    echo "Missing <$ENV_FILE> file!! Please double check with the setup guide (SETUP.md) again."
+    exit 1
 else
-  echo "Valid .env file found! Process to the next step..."
+    echo "Found <$ENV_FILE> file!! Move on to the next step..."
 fi
 
-if [ ! -f "$YAML_FILE" ]; then
-  echo "Invalid .yaml file found! Consider create one on your own based on the repository's file format and try again."
-  exit 1
+
+echo '=========================================================='
+echo "Setup docker service for [$BUILD_STAGE] build stage..."
+echo '=========================================================='
+
+
+# Docker needs privilage access on Linux
+if [ $(uname) == "Linux" ]; then
+    sudo docker compose --file "$YAML_FILE" --env-file "$ENV_FILE" up --build -d
+    echo "Finish setup docker service for [$BUILD_STAGE] build stage."
+
+# Docker has privilege access by default on Windows
 else
-  echo "Valid .yaml file found! Process to the next step..."
+    docker compose --file "$YAML_FILE" --env-file "$ENV_FILE" up --build -d
+    echo "Finish setup docker service for [$BUILD_STAGE] build stage."
 fi
-
-export BUILD_STAGE
-
-echo "Setting up Docker for <$BUILD_STAGE> phase..."
-
-docker-compose -f "$YAML_FILE" --env-file "$ENV_FILE" up --build -d
-
-echo "Completed setting up Docker for <$BUILD_STAGE> phase."
