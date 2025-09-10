759c0edfca9bf6bd0ce9027a6fa06b4663aa3720
FaceWithDark <114546939+FaceWithDark@users.noreply.github.com>
2025-09-11 00:12:37 +1000

    Add access level system for certain setting features
    - The database design have some changes that requires me to change some
      CRUD codes to match the new design.
    
    - Remove the 'Setting' icon when user is not logged in as normally,
      setting can only be modify if the user logged in.
    
    - Adjust one of the <View> file name to match the new database design.

--- src/private/Views/Setting/StaffSettingView.php
+++
@@ -1,7 +0,0 @@
-<header>
-    <h1>Staff Setting</h1>
-</header>
-
-<section class="staff-setting-page">
-    <h1>COMING SOON!! (STAFF)</h1>
-</section>
---
+++ src/private/Views/Setting/TournamentSettingView.php
@@ -0,0 +1,7 @@
+<header>
+    <h1>Tournament Setting</h1>
+</header>
+
+<section class="tournament-setting-page">
+    <h1>COMING SOON!! (TOURNAMENT)</h1>
+</section>
