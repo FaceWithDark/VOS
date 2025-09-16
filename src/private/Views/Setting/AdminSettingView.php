d18690f03d4d7f8022283261ff1a5b3beeff788c
FaceWithDark <114546939+FaceWithDark@users.noreply.github.com>
2025-09-16 15:51:56 +1000

    Add some template for admin setting

--- src/private/Views/Setting/AdminSettingView.php
+++ src/private/Views/Setting/AdminSettingView.php
@@ -3,5 +3,23 @@
 </header>
 
 <section class="admin-setting-page">
-    <h1>COMING SOON!! (ADMIN)</h1>
+    <div class="admin-add-role-panel">
+        <h2>Add User Role</h2>
+
+        <form action="">
+            <label for="userId">ID: </label>
+            <input type="text" id="userId" name="userId" autofocus>
+            <br />
+
+            <label for="lname">Role:</label>
+            <input type="text" id="userRole" name="userRole">
+            <br />
+
+            <input type="submit" value="Add / Update">
+        </form>
+    </div>
+
+    <div class="admin-view-data-panel">
+        <p>COMING SOON!!</p>
+    </div>
 </section>
