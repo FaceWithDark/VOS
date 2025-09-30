cd3a7eac34dfd8e0968cb0128b45a5fa177042f0
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2025-09-30 16:00:58 +1000

    Fix MVC structure for accessing entry point file

--- src/private/Controllers/AuthoriseController.php
+++ src/private/Controllers/AuthoriseController.php
@@ -5,26 +5,33 @@ declare(strict_types=1);
 require __DIR__ . '/../Configurations/Environment.php';
 
 
-$applicationId
-    =  getenv(name: 'APPLICATION_ID', local_only: true)
-    ?: getenv(name: 'APPLICATION_ID');
-$applicationRedirectUrl
-    =  getenv(name: 'APPLICATION_REDIRECT_URL', local_only: true)
-    ?: getenv(name: 'APPLICATION_REDIRECT_URL');
-$applicationResponseType
-    =  getenv(name: 'APPLICATION_RESPONSE_TYPE', local_only: true)
-    ?: getenv(name: 'APPLICATION_RESPONSE_TYPE');
-$applicationScope
-    =  getenv(name: 'APPLICATION_SCOPE', local_only: true)
-    ?: getenv(name: 'APPLICATION_SCOPE');
-$applicationState
-    =  getenv(name: 'APPLICATION_STATE', local_only: true)
-    ?: getenv(name: 'APPLICATION_STATE');
+if (
+    isset($_COOKIE['vot_access_id']) &&
+    isset($_COOKIE['vot_access_token'])
+) {
+    echo '<a href="/home">Seriously? Are not you already logged in bro??</a>';
+} else {
+    $applicationId
+        =  getenv(name: 'APPLICATION_ID', local_only: true)
+        ?: getenv(name: 'APPLICATION_ID');
+    $applicationRedirectUrl
+        =  getenv(name: 'APPLICATION_REDIRECT_URL', local_only: true)
+        ?: getenv(name: 'APPLICATION_REDIRECT_URL');
+    $applicationResponseType
+        =  getenv(name: 'APPLICATION_RESPONSE_TYPE', local_only: true)
+        ?: getenv(name: 'APPLICATION_RESPONSE_TYPE');
+    $applicationScope
+        =  getenv(name: 'APPLICATION_SCOPE', local_only: true)
+        ?: getenv(name: 'APPLICATION_SCOPE');
+    $applicationState
+        =  getenv(name: 'APPLICATION_STATE', local_only: true)
+        ?: getenv(name: 'APPLICATION_STATE');
 
-$applicationUrl = "https://osu.ppy.sh/oauth/authorize?client_id=$applicationId&redirect_uri=$applicationRedirectUrl&response_type=$applicationResponseType&scope=$applicationScope&state=$applicationState";
+    $applicationUrl = "https://osu.ppy.sh/oauth/authorize?client_id=$applicationId&redirect_uri=$applicationRedirectUrl&response_type=$applicationResponseType&scope=$applicationScope&state=$applicationState";
 
-exit(header(
-    header: "Location: $applicationUrl",
-    replace: true,
-    response_code: 302
-));
+    exit(header(
+        header: "Location: $applicationUrl",
+        replace: true,
+        response_code: 302
+    ));
+}
