03a803d73277fa94a980a194bc2a60bbe74b041f
FaceWithDark <114546939+FaceWithDark@users.noreply.github.com>
2025-10-01 12:41:57 +1000

    Fix MVC structure for different pages view based on set roles

---
+++ src/private/Controllers/Setting/AdminSettingController.php
@@ -0,0 +1,34 @@
+<?php
+# Not so much like static types, but at least it does feel better having this here
+declare(strict_types=1);
+
+
+if (
+    !isset($_COOKIE['vot_access_id']) &&
+    !isset($_COOKIE['vot_access_token'])
+) {
+    $adminSettingPath = explode(
+        separator: '/',
+        string: trim(
+            string: $_SERVER['REQUEST_URI'],
+            characters: '/'
+        )
+    )[1];
+
+    error_log(
+        message: sprintf(
+            "Suspicious attempt to access [%s] setting page detected!!",
+            strtoupper(string: $adminSettingPath)
+        ),
+        message_type: 0
+    );
+
+    exit(header(
+        header: 'Location: /home',
+        replace: true,
+        response_code: 302
+    ));
+} else {
+    require __DIR__ . '/../../Controllers/NavigationBarController.php';
+    require __DIR__ . '/../../Views/Setting/AdminSettingView.php';
+}
