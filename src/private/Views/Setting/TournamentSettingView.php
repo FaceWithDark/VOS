00df6a87b3fdefcf71db09dd720eab1218b1c302
FaceWithDark <114546939+FaceWithDark@users.noreply.github.com>
2025-10-29 13:35:42 +1100

    Update on current template for add mappool data from tournament setting page
    - Change to dynamically choose from which tournament and round that the
      host can add new mappool data to.
    - Somehow I start to have positive feeling with JS...

--- src/private/Views/Setting/TournamentSettingView.php
+++ src/private/Views/Setting/TournamentSettingView.php
@@ -3,32 +3,6 @@
 </header>
 
 <section class="tournament-setting-page">
-    <div class="tournament-add-mappool-panel">
-        <h2>Add mappool data for specific round and tournament</h2>
-
-        <form action="/setting/tournament" target="_self" method="post">
-            <label for="beatmapId">Beatmap ID</label>
-            <input type="text" name="beatmapId" placeholder="E.G: 2264801" autofocus required />
-            <br />
-
-            <label for="roundId">Round ID</label>
-            <input type="text" name="roundId" placeholder="E.G: QLF" autofocus required />
-            <br />
-
-            <label for="tournamentId">Tournament ID</label>
-            <input type="text" name="tournamentId" placeholder="E.G: VOT5" autofocus required />
-            <br />
-
-            <label for="beatmapType">Beatmap Type</label>
-            <input type="text" name="beatmapType" placeholder="E.G: NM1" autofocus required />
-            <br />
-
-            <input type="submit" value="Add Mappool Data">
-        </form>
-    </div>
-
-    <div class="tournament-view-data-panel">
-        <h2>View Mappool Data</h2>
-        <p>COMING SOON!!</p>
-    </div>
+    <!-- JS file -->
+    <script type="text/javascript" src="/assets/js/role/tournamentPanel.js"></script>
 </section>
