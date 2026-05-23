541f280529a6f22b038acccaef29df6dbdc11e9f
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-05-23 12:24:02 +1000

    init: new repo structure powered by Symfony framework

--- src/private/Controllers/Tournament/Vot4Controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-# Not so much like static types, but at least it does feel better having this here
-declare(strict_types=1);
-
-require __DIR__ . '/../NavigationBarController.php';
-require __DIR__ . '/../../Views/Tournament/GeneralVot4View.php';
