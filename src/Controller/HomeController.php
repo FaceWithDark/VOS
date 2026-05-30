679031ae3dcdceccbabb3d6bfbe467d92dc8abdc
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-05-30 19:20:05 +1000

    feat: add Home page (temp UX/UI design)
    The design you see under `/home` endpoint is generated from using
    Symfony Maker. It's not the final look but for the purpose of attached
    ticket scope, it's a valid to do and commit to create.
    
    Also, there's a test file created along this commit to perform an
    Application Test using PHPUnit.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+final class HomeController extends AbstractController
+{
+    #[Route(
+        path: '/home',
+        name: 'home_index',
+        methods: ['GET']
+    )]
+    public function index(): Response
+    {
+        return $this->render(
+            view: 'home/index.html.twig',
+            parameters: [
+                'controller_name' => 'HomeController',
+            ]
+        );
+    }
+}
