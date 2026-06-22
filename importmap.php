dd887644c41a22051d851030fdf3c6e7fd96382c
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-06-22 12:00:20 +1000

    chore: add UX Twig & Live Component packages
    Also adding `@source` Tailwind directives so that it can keep track of
    our asset files.

--- importmap.php
+++ importmap.php
@@ -25,4 +25,7 @@ return [
     '@hotwired/turbo' => [
         'version' => '8.0.23',
     ],
+    '@symfony/ux-live-component' => [
+        'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
+    ],
 ];
