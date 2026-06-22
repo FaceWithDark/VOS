6663b81d948c68c2954363076473983eaafd35ae
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-06-22 13:55:07 +1000

    feat: add base Card Component design
    Also set a default `source` path and more `@source` directives so
    Tailwind knows which extra files need to be keeping track off.

---
+++ src/Twig/Components/Card/BaseCard.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Twig\Components\Card;
+
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+
+#[AsTwigComponent]
+final class BaseCard {}
---
+++ src/Twig/Components/Card/TourneyCard.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Twig\Components\Card;
+
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+
+#[AsLiveComponent]
+final class TourneyCard
+{
+	use DefaultActionTrait;
+}
