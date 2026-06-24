a4636e0353d701a5b61d3daa819c1cda21007c48
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-06-24 21:34:41 +1000

    feat: move current navigation design to its own Twig Component
    Along with minor coding fixes due to my silly typos :)

---
+++ src/Twig/Components/Navigation/BaseBar.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Twig\Components\Navigation;
+
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+
+#[AsTwigComponent]
+final class BaseBar {}
---
+++ src/Twig/Components/Navigation/SideBar.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Twig\Components\Navigation;
+
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+
+#[AsLiveComponent]
+final class SideBar
+{
+	use DefaultActionTrait;
+}
