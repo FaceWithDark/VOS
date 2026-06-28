4845c79b2f598f775f390a3eeaf99e57aadb20bb
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-06-28 11:45:52 +1000

    feat: add API Platform and override some default values in the Bundle Twig templates to match our format

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
