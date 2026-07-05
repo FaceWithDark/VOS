e1a2bb3f4243cab14f64431723a0149a80ea2a8e
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-07-05 22:12:50 +1000

    feat: add generated Entity classes on ERD to match tables on ERD for each base domain database schema

---
+++ src/Repository/Web/RoleRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Repository\Web;
+
+use App\Entity\Web\Role;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+
+/**
+ * @extends ServiceEntityRepository<Role>
+ */
+class RoleRepository extends ServiceEntityRepository
+{
+	public function __construct(ManagerRegistry $registry)
+	{
+		parent::__construct($registry, Role::class);
+	}
+
+	//    /**
+	//     * @return Role[] Returns an array of Role objects
+	//     */
+	//    public function findByExampleField($value): array
+	//    {
+	//        return $this->createQueryBuilder('r')
+	//            ->andWhere('r.exampleField = :val')
+	//            ->setParameter('val', $value)
+	//            ->orderBy('r.id', 'ASC')
+	//            ->setMaxResults(10)
+	//            ->getQuery()
+	//            ->getResult()
+	//        ;
+	//    }
+
+	//    public function findOneBySomeField($value): ?Role
+	//    {
+	//        return $this->createQueryBuilder('r')
+	//            ->andWhere('r.exampleField = :val')
+	//            ->setParameter('val', $value)
+	//            ->getQuery()
+	//            ->getOneOrNullResult()
+	//        ;
+	//    }
+}
