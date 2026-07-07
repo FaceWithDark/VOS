82e544c606e80abdd669ae07c3de5f39e3c4e0e9
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-07-07 19:22:05 +1000

    feat: automatically run migration scripts from Symfony container rather than manually execute the command

---
+++ migrations/Utility/Migration.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# NOTE:
+# On production:
+# 1. Restrict to hosting platform IP and remove `--allow-all-ip` flag
+# 2. Generate SQL files and execute manually to ensure attackers not assuming we
+#    run migration scripts by default
+symfony console doctrine:migrations:migrate -n
+symfony --allow-all-ip local:server:start
