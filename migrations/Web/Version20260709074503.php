2d9d909dadcc93726f220266b2400b6d58a61c78
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-07-09 18:21:16 +1000

    feat: add migration scripts to create default catalog schema for managing registered tournaments under VOS org

---
+++ migrations/Web/Version20260709074503.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace DoctrineMigrations\Web;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+
+final class Version20260709074503 extends AbstractMigration
+{
+	private string $dbUser;
+	private string $name		= 'VOS';
+	private string $schema		= '';
+	private string $comment		= '';
+	private string $statement	= '';
+
+	public function __construct()
+	{
+		$this->dbUser
+			=  $_ENV['DB_USER']
+			?? getenv('DB_USER')
+			?: 'demo';
+		$this->schema	= sprintf(
+			'%s_CATALOG',
+			$this->name
+		);
+		$this->comment	= sprintf(
+			'%s base domain schema.',
+			$this->name
+		);
+	}
+
+	public function getDescription(): string
+	{
+		return sprintf(
+			'Create catalog schema to better categorise registered tournaments under %s org.',
+			$this->name
+		);
+	}
+
+	public function up(Schema $schema): void
+	{
+		// This schema need to be created first
+		$this->statement =
+			<<<"SQL"
+			CREATE SCHEMA IF NOT EXISTS
+				{$this->schema}
+			AUTHORIZATION
+				"{$this->dbUser}"
+			SQL;
+
+		$this->addSql(sql: $this->statement);
+
+
+		// Then we can add comment on the schema since it's existed now
+		$this->statement =
+			<<<"SQL"
+			COMMENT ON SCHEMA
+				{$this->schema}
+			IS
+				'{$this->comment}'
+			SQL;
+
+		$this->addSql(sql: $this->statement);
+	}
+
+	public function down(Schema $schema): void
+	{
+		$this->statement =
+			<<<"SQL"
+			DROP SCHEMA IF EXISTS {$this->schema} CASCADE
+			SQL;
+
+		$this->addSql(sql: $this->statement);
+	}
+}
