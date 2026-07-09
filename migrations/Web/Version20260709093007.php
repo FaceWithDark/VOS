7cc4ac5167a06354c4f9aa39fe096a690a6f677e
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-07-09 19:45:37 +1000

    chore: create the `messenger_messages` table under the new catalog schema as generated by Doctrine Migration command

---
+++ migrations/Web/Version20260709093007.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace DoctrineMigrations\Web;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+
+final class Version20260709093007 extends AbstractMigration
+{
+	private string $name		= 'VOS';
+	private string $schema		= '';
+	private string $statement	= '';
+
+	public function __construct()
+	{
+		$this->schema	= sprintf(
+			'%s_CATALOG',
+			$this->name
+		);
+	}
+
+	public function getDescription(): string
+	{
+		return sprintf(
+			'Create `messenger_messages` table for %s catalog schema under %s org.',
+			$this->schema,
+			$this->name
+		);
+	}
+
+	public function up(Schema $schema): void
+	{
+		// This table need to be created first
+		$this->statement =
+			<<<"SQL"
+			CREATE TABLE IF NOT EXISTS {$this->schema}.messenger_messages (
+				id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+				body TEXT NOT NULL,
+				headers TEXT NOT NULL,
+				queue_name VARCHAR(190) NOT NULL,
+				created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+				available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+				delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL,
+				CONSTRAINT PK_MESSENGER_MESSAGE_ID PRIMARY KEY (id)
+			)
+			SQL;
+
+		$this->addSql(sql: $this->statement);
+
+
+		// Not too sure why Doctrine Migration commands generate this, but
+		// we'll just be safe and keep it as it is
+		$this->statement =
+			<<<"SQL"
+			CREATE INDEX
+				IDX_MESSENGER_MESSAGE_ID
+			ON
+				{$this->schema}.messenger_messages (
+					queue_name,
+					available_at,
+					delivered_at,
+					id
+				)
+			SQL;
+
+		$this->addSql(sql: $this->statement);
+	}
+
+	public function down(Schema $schema): void
+	{
+		$this->statement =
+			<<<"SQL"
+			DROP TABLE IF EXISTS {$this->schema}.messenger_messages CASCADE;
+			SQL;
+
+		$this->addSql(sql: $this->statement);
+	}
+}
