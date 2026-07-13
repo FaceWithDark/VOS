4ddae343ece78d536dcc1d6812c784bb64b4474a
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-07-13 23:36:20 +1000

    chore: update from mutable datetime type to immutable datetime type + add the missing attribute to allow auto-increment `id` column
    as recommended from both PHP and Symfony docs.

--- src/Entity/Abstract/AbstractUser.php
+++ src/Entity/Abstract/AbstractUser.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 
 namespace App\Entity\Abstract;
 
+use DateTimeImmutable;
+use DateTimeZone;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\DBAL\Types\Types;
 
@@ -19,10 +21,18 @@ abstract class AbstractUser {
 	protected ?int $id = null;
 
 	#[ORM\Column(
-		type: Types::DATETIMETZ_MUTABLE,
+		type: Types::DATETIMETZ_IMMUTABLE,
 		nullable: false
 	)]
-	protected ?\DateTime $createOn = null;
+	protected ?\DateTimeImmutable $createOn = null;
+
+	public function __construct()
+	{
+		$this->createOn = new DateTimeImmutable(
+			datetime: 'now',
+			timezone: new DateTimeZone(timezone: 'UTC')
+		);
+	}
 
 	protected function getId(): ?int
 	{
@@ -36,12 +46,12 @@ abstract class AbstractUser {
 		return $this;
 	}
 
-	protected function getCreateOn(): ?\DateTime
+	protected function getCreateOn(): ?\DateTimeImmutable
 	{
 		return $this->createOn;
 	}
 
-	protected function setCreateOn(\DateTime $createOn): static
+	protected function setCreateOn(\DateTimeImmutable $createOn): static
 	{
 		$this->createOn = $createOn;
 
