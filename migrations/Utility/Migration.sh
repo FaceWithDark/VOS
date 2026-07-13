3d32c8bda68114f19a13fad19aa326cf9b46dce7
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-07-13 13:32:29 +1000

    chore: typo spotted and remove the extra migration command since the order is correct now

--- migrations/Utility/Migration.sh
+++ migrations/Utility/Migration.sh
@@ -6,5 +6,4 @@
 # 2. Generate SQL files and execute manually to ensure attackers not assuming we
 #    run migration scripts by default
 symfony console doctrine:migrations:migrate --no-interaction
-symfony console doctrine:migrations:execute "$PWD/migrations/Version20260712010015.php" --up --no-interaction
 symfony --allow-all-ip local:server:start
